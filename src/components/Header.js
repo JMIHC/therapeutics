06d147b759525740797dae608fc3538d229d6992
jmihc <JMIHC@users.noreply.github.com>
2019-08-11 17:13:30 -0500

    working on background mobile view

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div>
-
-        </div>
-        <div className="content">
-            <div className="inner">
-                <h1>Cornyn Therapeutics, LTD</h1>
-                <p>Welcome, Intro here</p>
-            </div>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Schedule a session</a></li>
-            </ul>
-        </nav>
-    </header>
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="content">
+      <div className="inner">
+        <h1>Cornyn Therapeutics, LTD</h1>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Schedule a session
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 )
 
 Header.propTypes = {
-    onOpenArticle: PropTypes.func,
-    timeout: PropTypes.bool
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool,
 }
 
 export default Header
