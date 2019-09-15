e43ca84a514677712901443c3151a4c0fc96ef99
jmihc <JMIHC@users.noreply.github.com>
2019-09-15 11:50:38 -0500

    refinement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
     <p style={{ fontSize: 10 }} className="copyright">
       Design by: <a href="https://html5up.net">HTML5 UP</a>. Brought to you by:{' '}
-      <a href="https://cosmicfisherman.tech">Cosmic Fisherman</a>.
+      <a href="https://cosmicfisherman.com">Cosmic Fisherman</a>.
     </p>
   </footer>
 )
